80c5ac5d8c4f864dd457ddca8fd4d0cb91329205
Ringlex <bercik.1122@wp.pl>
2020-12-08 19:57:36 +0100

    Modify drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -9,12 +9,14 @@ class AppDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
-        color: Colors.white,
+        color: Colors.green[600],
         child: Column(
           children: <Widget>[
-            AppBar(
-              backgroundColor: Colors.green[900],
-              title: Text(
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Text(
                 'Hello Friend!',
                 style: TextStyle(
                   color: Colors.white,
@@ -22,118 +24,144 @@ class AppDrawer extends StatelessWidget {
                   fontSize: 30,
                 ),
               ),
-              automaticallyImplyLeading: false,
             ),
-            Divider(),
-            ListTile(
-              leading: Container(
+            
+            SingleChildScrollView(
+              child: Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(90),
-                  border: Border.all(
-                    color: Colors.grey[800],
-                    width: 2,
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(220),
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(220),
                   ),
                 ),
-                child: Icon(
-                  Icons.home,
-                  color: Colors.grey[800],
-                  size: 30,
-                ),
-              ),
-              title: Text(
-                'Home',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              onTap: () {
-                Navigator.of(context).pushReplacementNamed(News.routeName);
-              },
-            ),
-            Divider(),
-            ListTile(
-              leading: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(90),
-                    border: Border.all(color: Colors.grey[800], width: 2)),
-                child: Icon(
-                  Icons.settings,
-                  color: Colors.grey[800],
-                  size: 30,
-                ),
-              ),
-              title: Text(
-                'Settings',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              onTap: () {
-                // Navigator.of(context)
-                //     .pushReplacementNamed(OrdersScreen.routeName);
-                // Navigator.of(context).pushReplacement(
-                //   CustomRoute(
-                //     builder: (ctx) => OrdersScreen(),
-                //   ),
-                // );
-              },
-            ),
-            Divider(),
-            ListTile(
-              leading: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(90),
-                      border: Border.all(color: Colors.grey[800], width: 2)),
-                  child: Icon(
-                    Icons.person,
-                    color: Colors.grey[800],
-                    size: 30,
-                  )),
-              title: Text(
-                'My Account',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              onTap: () {
-                //MyAccount
-                Navigator.of(context).pushReplacementNamed(MyAccount.routeName);
-              },
-            ),
-            Divider(),
-            ListTile(
-              leading: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(90),
-                      border: Border.all(color: Colors.grey[800], width: 2)),
-                  child: Icon(
-                    Icons.exit_to_app,
-                    color: Colors.grey[800],
-                    size: 30,
-                  )),
-              title: Text(
-                'Logout',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              onTap: () {
-                FirebaseAuth.instance.signOut();
-                Navigator.of(context).pop();
-                Navigator.of(context).pushReplacementNamed('/');
+                height: MediaQuery.of(context).size.height - 90,
+                //color: Colors.white,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 140,
+                    ),
+                    ListTile(
+                      leading: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(90),
+                          border: Border.all(
+                            color: Colors.grey[800],
+                            width: 2,
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.home,
+                          color: Colors.grey[800],
+                          size: 30,
+                        ),
+                      ),
+                      title: Text(
+                        'Home',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.of(context)
+                            .pushReplacementNamed(News.routeName);
+                      },
+                    ),
+                    Divider(),
+                    ListTile(
+                      leading: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(90),
+                            border:
+                                Border.all(color: Colors.grey[800], width: 2)),
+                        child: Icon(
+                          Icons.settings,
+                          color: Colors.grey[800],
+                          size: 30,
+                        ),
+                      ),
+                      title: Text(
+                        'Settings',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onTap: () {
+                        // Navigator.of(context)
+                        //     .pushReplacementNamed(OrdersScreen.routeName);
+                        // Navigator.of(context).pushReplacement(
+                        //   CustomRoute(
+                        //     builder: (ctx) => OrdersScreen(),
+                        //   ),
+                        // );
+                      },
+                    ),
+                    Divider(),
+                    ListTile(
+                      leading: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(90),
+                              border: Border.all(
+                                  color: Colors.grey[800], width: 2)),
+                          child: Icon(
+                            Icons.person,
+                            color: Colors.grey[800],
+                            size: 30,
+                          )),
+                      title: Text(
+                        'My Account',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onTap: () {
+                        //MyAccount
+                        Navigator.of(context)
+                            .pushReplacementNamed(MyAccount.routeName);
+                      },
+                    ),
+                    Divider(),
+                    ListTile(
+                      leading: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(90),
+                              border: Border.all(
+                                  color: Colors.grey[800], width: 2)),
+                          child: Icon(
+                            Icons.exit_to_app,
+                            color: Colors.grey[800],
+                            size: 30,
+                          )),
+                      title: Text(
+                        'Logout',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onTap: () {
+                        FirebaseAuth.instance.signOut();
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pushReplacementNamed('/');
 
-                // Navigator.of(context)
-                //     .pushReplacementNamed(UserProductsScreen.routeName);
-                // Provider.of<Auth>(context, listen: false).logout();
-              },
+                        // Navigator.of(context)
+                        //     .pushReplacementNamed(UserProductsScreen.routeName);
+                        // Provider.of<Auth>(context, listen: false).logout();
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
