b11f36d8e2d7406dc1710b200d5957b491e98a25
Ringlex <bercik.1122@wp.pl>
2020-12-13 16:13:29 +0100

    Adding picture of current user to his posts

--- lib/screens/news.dart
+++ lib/screens/news.dart
@@ -1,6 +1,7 @@
 import 'package:bonsai_app/model/news_data.dart';
 import 'package:bonsai_app/widgets/news_item.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../widgets/app_drawer.dart';
 
@@ -13,9 +14,10 @@ class News extends StatefulWidget {
 
 class _NewsState extends State<News> {
   //final displayedNews = DUMMY_NEWS;
-
+  
   @override
   Widget build(BuildContext context) {
+    
 
 Future<int> countDocuments() async {
     QuerySnapshot _myDoc = await FirebaseFirestore.instance.collection('posts').get();
--- lib/widgets/news_item.dart
+++ lib/widgets/news_item.dart
@@ -1,11 +1,13 @@
 import 'package:bonsai_app/auth_data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class NewsItem extends StatelessWidget {
   final String id;
   final String description;
   final bool isFavorite;
-  final String imageUrl;
+  String imageUrl;
 
   NewsItem({
     @required this.id,
@@ -15,63 +17,79 @@ class NewsItem extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        elevation: 4,
-        margin: EdgeInsets.all(10),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              leading: CircleAvatar(
-                child: Icon(
-                  Icons.supervised_user_circle,
-                ),
+
+    final uid = FirebaseAuth.instance.currentUser.uid;
+    var pict;
+
+    return FutureBuilder(
+        future: FirebaseFirestore.instance.collection('users').doc(uid).get(),
+        builder: (context,
+             snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Text(' '),
+            );
+          }
+          pict = snapshot.data['userPicture'];
+          return InkWell(
+            onTap: () {},
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
               ),
-              title: Text(authData[0].username),
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15),
-              ),
-              child: Image.network(
-                imageUrl,
-                height: 250,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Icon(Icons.favorite_border),
-                        Icon(Icons.mode_comment),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Divider(),
-                    SizedBox(height: 20),
-                    Container(
-                      child: Text(
-                        description,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
+              elevation: 4,
+              margin: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        pict,
+                        //'https://youraverageguystyle.com/wp-content/uploads/2018/10/Mens-Fashion-Blogger-ASOS-Grey-T-Shirt-Aviators-Teal-Orange.jpg',
                       ),
-                    )
-                  ],
-                ))
-          ],
-        ),
-      ),
-    );
+                    ),
+                    title: Text(authData[0].username),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                    child: Image.network(
+                      imageUrl.toString(),
+                      height: 250,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Icon(Icons.favorite_border),
+                              Icon(Icons.mode_comment),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Divider(),
+                          SizedBox(height: 20),
+                          Container(
+                            child: Text(
+                              description,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      ))
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
